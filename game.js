1a2d0a04b783c4732f5e4fabd50c244213cf3251
Ross Clettenberg <raclettenberg@gmail.com>
2022-03-09 10:13:48 -0600

    Initial commit. Changed text at the top. Added jQuery and JavaScript scripts to HTML file. Created game.js file. Added JavaScript alert, and random number function.

---
+++ game.js
@@ -0,0 +1,14 @@
+alert("test");
+
+function nextSequence() {
+
+var randomNumber = Math.random();
+
+randomNumber = randomNumber * 3;
+
+randomNumber = Math.floor(randomNumber);
+
+console.log(randomNumber);
+
+}
+
